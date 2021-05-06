eee04436037fc2658548992e13204221b63f7a8f
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-05-06 20:31:57 +0300

    Changed function to async for fetching data cleanly.

--- Day 10 - Dad Jokes/script.js
+++ Day 10 - Dad Jokes/script.js
@@ -5,13 +5,15 @@ jokeBtn.addEventListener("click",generateJoke)
 
 generateJoke()
 
-function generateJoke() {
+async function generateJoke() {
     const config = {
         headers : {
             Accept : "application/json",
         },
     }
-    fetch("https://icanhazdadjoke.com", config).then((res) => res.json()).then((data) => {
-        jokeElement.innerHTML = data.joke
-    })
+    const res = await fetch("https://icanhazdadjoke.com", config)
+    
+    const data = await res.json()
+
+    jokeElement.innerHTML = data.joke
 }
