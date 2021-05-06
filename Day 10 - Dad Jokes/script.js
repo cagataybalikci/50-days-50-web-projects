235d78db20cbe073ce7d2e7c53fb25ba539f10be
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-05-06 20:25:53 +0300

    Dad Joke API fetched.

--- Day 10 - Dad Jokes/script.js
+++ Day 10 - Dad Jokes/script.js
@@ -1,1 +1,17 @@
+const jokeElement = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
 
+jokeBtn.addEventListener("click",generateJoke)
+
+generateJoke()
+
+function generateJoke() {
+    const config = {
+        headers : {
+            Accept : "application/json",
+        },
+    }
+    fetch("https://icanhazdadjoke.com", config).then((res) => res.json()).then((data) => {
+        jokeElement.innerHTML = data.joke
+    })
+}
