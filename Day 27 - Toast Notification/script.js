00c05f849e9d109af276d3f32e08c02a9b358166
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-06-12 10:05:58 +0300

    Day 27 completed.

--- Day 27 - Toast Notification/script.js
+++ Day 27 - Toast Notification/script.js
@@ -1,1 +1,45 @@
+const button = document.getElementById("button")
+const toasts = document.getElementById("toasts")
 
+const messages = [
+    "Message One",
+    "Message Two",
+    "Message Three",
+    "Message Four "
+]
+
+const indicator = [
+    "success",
+    "warning",
+    "danger"
+]
+
+
+button.addEventListener("click",()=> createNotification())
+
+
+function createNotification() {
+    const notif = document.createElement("div")
+    notif.classList.add("toast")
+    const randomIndicator = getRandomIndicator()
+    console.log(randomIndicator);
+    notif.classList.add(randomIndicator)
+    console.log(notif.classList);
+
+    notif.innerText = getRandomMessage()
+
+    toasts.appendChild(notif)
+
+    setTimeout(() => {
+        notif.remove()
+    }, 3000)
+}
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
+}
+
+
+function getRandomIndicator() {
+    return indicator[Math.floor(Math.random() * indicator.length)]
+}
