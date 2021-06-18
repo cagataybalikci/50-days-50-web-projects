aab801f56e575ad9894719b593d2097d8936c0de
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-06-18 16:27:16 +0300

    Day 29 Js Completed.

--- Day 29 - Double Heart Click/script.js
+++ Day 29 - Double Heart Click/script.js
@@ -1,1 +1,44 @@
+const loveMe = document.querySelector(".loveMe")
+const times = document.querySelector("#times")
 
+let clickTime = 0
+let timesClicked = 0
+
+// You can also use dblclick much more easier
+loveMe.addEventListener("click",(e)=>{
+    if (clickTime  === 0) {
+        clickTime = new Date().getTime()
+    }else {
+        if ((new Date().getTime() - clickTime) < 800) {
+            createHeart(e)
+            clickTime = 0
+        }else{
+            clickTime = new Date().getTime()
+        }
+    }
+})
+
+
+const createHeart = (e) => {
+    const heart = document.createElement("i")
+    heart.classList.add("fas")
+    heart.classList.add("fa-heart")
+
+    const x = e.clientX
+    const y = e.clientY
+
+    const offsetX = e.target.offsetLeft
+    const offsetY = e.target.offsetTop
+
+    const insideX = x - offsetX
+    const insideY = y - offsetY
+
+    heart.style.top = `${insideY}px`
+    heart.style.left = `${insideX}px`
+
+    loveMe.appendChild(heart)
+
+    times.innerHTML = ++timesClicked
+    
+    setTimeout(()=> heart.remove(),1000)
+}
