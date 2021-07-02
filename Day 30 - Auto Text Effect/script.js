7b50ad8ba0d42aed7fc6f1aac6c22aa6cd210608
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-07-02 12:11:36 +0300

    Day 30 Js Completed.

--- Day 30 - Auto Text Effect/script.js
+++ Day 30 - Auto Text Effect/script.js
@@ -1,1 +1,25 @@
+const textElement = document.getElementById("text")
+const speedElement = document.getElementById("speed")
 
+const textContent = 'We Love Programming!'
+
+let idx = 1
+
+let speed = 300 / speedElement.value
+
+
+writeText()
+
+function writeText(){
+    textElement.innerText = textContent.slice(0,idx)
+
+    idx++
+
+    if(idx > textContent.length){
+        idx = 1
+    }
+
+    setTimeout(writeText,speed)
+}
+
+speedElement.addEventListener("input",(e)=> speed = 300 / e.target.value)
