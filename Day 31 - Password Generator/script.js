fe8e1c4788e7ca724678da247744279ea29bf56c
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-07-02 13:27:04 +0300

    Day 31 JS Completed.

--- Day 31 - Password Generator/script.js
+++ Day 31 - Password Generator/script.js
@@ -1,1 +1,81 @@
+const resultElement = document.getElementById("result");
+const lengthElement = document.getElementById("length");
+const uppercaseElement = document.getElementById("uppercase");
+const lowercaseElement = document.getElementById("lowercase");
+const numberElement = document.getElementById("numbers");
+const symbolElement = document.getElementById("symbols");
+const generateElement = document.getElementById("generate");
+const clipboardElement = document.getElementById("clipboard");
 
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+generateElement.addEventListener("click", () => {
+  const length = +lengthElement.value;
+  const hasLower = lowercaseElement.checked;
+  const hasUpper = uppercaseElement.checked;
+  const hasNumber = numberElement.checked;
+  const hasSymbol = symbolElement.checked;
+
+  resultElement.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+clipboardElement.addEventListener("click",()=>{
+    const textarea = document.createElement("textarea")
+    const password = resultElement.innerText
+
+    if (!password){return}
+
+    textarea.value = password
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
+    alert("Password copied to the clipboard!")
+})
+
+function generatePassword(lower, upper, number, symbol,length) {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArray = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArray.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+  return finalPassword;
+}
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol() {
+  const symbols = "!@#$^½*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
