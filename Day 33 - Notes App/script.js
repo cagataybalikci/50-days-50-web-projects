88721eccda77cd3c29a1f62168d594efb5c064a6
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-07-05 13:38:32 +0300

    Day 33 Local Storage added.

--- Day 33 - Notes App/script.js
+++ Day 33 - Notes App/script.js
@@ -1,5 +1,11 @@
 const addBtn = document.getElementById("add");
 
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if(notes){
+    notes.forEach(note => addNewNote(note))
+}
+
 addBtn.addEventListener("click", () => {
   addNewNote();
 });
@@ -20,27 +26,41 @@ function addNewNote(text = "") {
         <textarea class="${text ? "hidden" : ""}"></textarea>
     `;
 
-    const editBtn = note.querySelector(".edit")
-    const deleteBtn = note.querySelector(".delete")
-    const main = note.querySelector(".main")
-    const textArea = note.querySelector("textarea")
+  const editBtn = note.querySelector(".edit");
+  const deleteBtn = note.querySelector(".delete");
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
 
-    textArea.value = text
-    main.innerHTML = marked(text)
+  textArea.value = text;
+  main.innerHTML = marked(text);
 
-    deleteBtn.addEventListener("click",()=>{
-        note.remove()
-    })
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+    updateLS();
+  });
 
-    editBtn.addEventListener("click",()=>{
-        main.classList.toggle("hidden")
-        textArea.classList.toggle("hidden")
-    })
+  editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
 
-    textArea.addEventListener('input',(e)=>{
-        text = e.target.value
-        main.innerHTML = marked(text)
-    })
+  textArea.addEventListener("input", (e) => {
+    text = e.target.value;
+    main.innerHTML = marked(text);
+    updateLS();
+  });
 
-    document.body.appendChild(note)
+  document.body.appendChild(note);
+}
+
+// Local Storage
+
+function updateLS() {
+  const notesText = document.querySelectorAll("textarea");
+
+  const notes = [];
+
+  notesText.forEach((note) => notes.push(note.value));
+
+  localStorage.setItem('notes',JSON.stringify(notes))
 }
