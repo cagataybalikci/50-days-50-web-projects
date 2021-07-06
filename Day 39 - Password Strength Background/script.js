52d3e240f49be423eba38d8e2f5b316b8ec66c7a
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-07-06 15:03:30 +0300

    Day 39 JS Completed.

--- Day 39 - Password Strength Background/script.js
+++ Day 39 - Password Strength Background/script.js
@@ -1,1 +1,14 @@
+const passwordField = document.getElementById("password");
+const bg = document.getElementById("background");
 
+passwordField.addEventListener("input", (e) => {
+  const inputLenght = e.value.length
+  
+  let blurValue = 20;
+
+  if (blurValue > 0) {
+    bg.style.filter = `blur(${valueForBlur - inputLenght * 2}px)`;
+  } else {
+    bg.style.filter = `blur(0px)`;
+  }
+});
